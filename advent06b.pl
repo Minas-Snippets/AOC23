1fa9e5c2b796cc9fb8ee318856039bb39da17161
Mina <76761228+Minas-Snippets@users.noreply.github.com>
2023-12-06 14:14:32 +0100

    Update advent06b.pl shortened

--- advent06b.pl
+++ advent06b.pl
@@ -7,9 +7,5 @@ while(<>) {
         $t = $_/2;
         next
     }
-    my $r = sqrt($t*$t -$_);
-    my ($x1, $x2) = ($t - $r, $t + $r);
-    $x1 = int($x1 + 1);
-    $x2 = $x2 == int($x2) ? int($x2 - 1) : int($x2);
-    print ($x2 - $x1 + 1);    
+    print 2*$t - 2*int($t - sqrt($t*$t -$_) + 1) + 1   
 }
